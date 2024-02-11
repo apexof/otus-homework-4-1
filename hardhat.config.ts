446a4cfbc7e758c73b1add1f43c1525ca14df7e5
AlexOrekhov <apexof@gmail.com>
2024-02-11 19:28:36 +0300

    save

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,8 +4,6 @@ dotEnvConfig();
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
 
-
-
 const config: HardhatUserConfig = {
   solidity: {
     version: "0.8.23",
